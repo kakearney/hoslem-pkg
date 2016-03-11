2f35e6abe5f574be82bb1d60c303b2c8ae6a4547
Kelly Kearney <kakearney@gmail.com>
2016-03-10 17:28:48 -0800

    Initial commit for standalone repo: hoslem
    Due to a technical mistake, I'm changing the way I manage my tiny
    repos to use multiple standalone repos rather than one large
    subtree-ed one.  While the code is the same as the old version,
    the commit history is being wiped out by this move.  Anyone who
    has cloned or forked this code (or any code including this one via
    subtrees) will need to rebase or reclone.  Sorry about that.

---
+++ hoslem.m
@@ -0,0 +1,93 @@
+function [p, S] = hoslem(varargin)
+%HOSLEM Hosmer-Lemshow goodness-of-fit test
+%
+% [p, S] = hoslem(ypred, y)
+% [p, S] = hoslem(mdl, x, y)
+% [p, S] = hoslem(..., ng)
+%
+% Input variables:
+%
+%   ypred:  Logistic regression predictions for each data point; values
+%           should be between 0 and 1
+%
+%   mdl:    GeneralizedLinearModel object
+%
+%   x:      values at which to evaluate the generalized linear model
+%
+%   y:      observed values at each data point, should be 0 or 1
+%
+% Output variables:
+%
+%   p:      goodness-of-fit statistic. If >0.05, model fits data acceptably
+%
+%   S:      structure with the following fields
+%           
+%           n:      number of points in each bin
+%
+%           obs:    observed number of positive observations in each bin
+%
+%           exx:    predicted number of positive observations in each bin
+%
+%           g:      chi-squared statistic
+
+% Copyright 2014 Kelly Kearney
+
+% Parse input
+
+isng = cellfun(@(x) isnumeric(x) && isscalar(x), varargin);
+if any(isng)
+    ng = varargin{isng};
+    varargin = varargin(~isng);
+else
+    ng = 10;
+end
+
+if isa(varargin{1}, 'GeneralizedLinearModel') && length(varargin) == 3
+    mdl = varargin{1};
+    x = varargin{2};
+    y = varargin{3};
+    ypred = predict(mdl, x);
+elseif isa(varargin{1}, 'GeneralizedLinearModel') && isa(varargin{2}, 'dataset')
+    mdl = varargin{1};
+    ds = varargin{2};
+    x = double(ds(:,mdl.PredictorNames));
+    y = double(ds(:,mdl.ResponseName));
+    ypred = predict(mdl, ds);
+elseif length(varargin) == 2
+    ypred = varargin{1};
+    y = varargin{2};
+else
+    error('Could not parse input');
+end
+
+
+% Determine bins
+
+prc = prctile(ypred, linspace(0,100,ng+1));
+[n, idx] = histc(ypred, prc);
+n(ng) = sum(n(ng:ng+1));
+n = n(1:ng);
+idx(idx == (ng+1)) = ng;
+
+% Group data into bins (there really shouldn't ever be empty bins, but if
+% the percentiles are really wonky, this can happen... usually due to
+% iteration limit error, but stil...)
+
+[agidx, yptmp] = aggregate(idx, ypred);
+[agidx, yotmp] = aggregate(idx, y);
+[yp, yo] = deal(cell(ng,1));
+yp(agidx) = yptmp;
+yo(agidx) = yotmp;
+
+obs = cellfun(@sum, yo);
+exx = cellfun(@sum, yp);
+
+g = sum(((obs - exx).^2)./(exx.*(1-exx./n)));
+p = 1 - chi2cdf(g, ng-2);
+
+% Output
+
+S.n = n;
+S.obs = obs;
+S.exx = exx;
+S.g = g;
